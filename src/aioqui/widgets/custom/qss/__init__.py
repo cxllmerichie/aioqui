991c1f93033d0bd4e3c9e9d619b3e10efc283b4c
Roman Lysychkin <cxllmerichie@gmail.com>
2023-06-10 01:42:44 +0200

    2.0.6

--- src/aioqui/widgets/custom/qss/__init__.py
+++ src/aioqui/widgets/custom/qss/__init__.py
@@ -2,3 +2,4 @@ from .date_time import qss as DateTime
 from .popup import qss as Popup
 from .searchbar import qss as SearchBar
 from .image_button import qss as ImageButton
+from .scrollbar import qss as ScrollBar
